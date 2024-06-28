edde7562dfe586f08ca73efd839aee679df76bcf
Kahlium <kahligtz@gmail.com>
2024-06-27 20:37:54 -0600

    book model up and replaced a few things from the mini project

--- models/index.js
+++ models/index.js
@@ -1,13 +1,21 @@
 const Reader = require('./Reader');
 const Review = require('./Review');
+const Book = require('./Book')
 
 Reader.hasMany(Review, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE'
+});
+Book.hasMany(Review, {
   foreignKey: 'reader_id',
   onDelete: 'CASCADE'
 });
 
 Review.belongsTo(Reader, {
   foreignKey: 'reader_id'
+});
+Review.belongsTo(Book, {
+  foreignKey: 'book_id'
 });
 
 module.exports = { Reader, Review };
