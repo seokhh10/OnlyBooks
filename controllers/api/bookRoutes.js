d948fc0d40a3c95420cd203d1754f5811fca5321
Kahlium <kahligtz@gmail.com>
2024-07-02 20:30:59 -0600

    books in homepage limited to 5, reviews not up yet

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
    try {
        const bookData = await Book.findAll();
        res.status(200).json(bookData);
-       res.render('bookList')
+       res.render('books')
    } catch (err) {
      res.status(500).json(err);
    }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,11 +9,14 @@ router.get('/', async (req, res) => {
 
     // Serialize data so the template can read it
     const books = bookData.map((Book) => Book.get({ plain: true }));
+    const bookLimit = books.slice(0, 5)
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      books, 
-      logged_in: req.session.logged_in 
+      bookLimit
+    });
+    res.render('bookList', { 
+      books
     });
   } catch (err) {
     res.status(500).json(err);
@@ -22,11 +25,11 @@ router.get('/', async (req, res) => {
 
 router.get('/book/:id', async (req, res) => {
   try {
-    const bookData = await Book.findByPk(req.params.id,);
+    const bookData = await Book.findByPk(req.params.id);
 
     const book = bookData.get({ plain: true });
 
-    res.render('Book', {
+    res.render('book', {
       ...book,
       logged_in: req.session.logged_in
     });
@@ -41,7 +44,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const readerData = await Reader.findByPk(req.session.reader_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Book }],
+      include: [{ model: Review }],
     });
 
     const reader = readerData.get({ plain: true });
