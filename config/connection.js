93f589ee52c512586bcb53f0aa227783d59b706b
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-07-02 22:25:14 -0500

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,8 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  // Using the provided database URL for production
-  sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // This is necessary to avoid SSL certificate validation errors
-      }
-    }
-  });
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
-  // Fallback to local development configuration
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -24,25 +13,8 @@ if (process.env.DB_URL) {
     {
       host: 'localhost',
       dialect: 'postgres'
-      }
+    }
   );
 }
 
 module.exports = sequelize;
-
-
-// if (process.env.DB_URL) {
-//   sequelize = new Sequelize(process.env.DB_URL);
-// } else {
-//     sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'postgres'
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
